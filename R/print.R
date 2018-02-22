2db92f48ac90868ae2a6ea7b91b2a26997e79880
Jongbin Jung <jungjongbin86@gmail.com>
2018-02-22 15:42:47 -0800

    Init print.undi()

---
+++ R/print.R
@@ -0,0 +1,8 @@
+#' \code{print} method of object class \code{\link{undi}}
+#'
+#' @export
+print.undi <- function(x) {
+  # TODO: Implement me
+  warning("TODO: print.undi() needs to be implemented properly")
+  print(x$coefs)
+}
