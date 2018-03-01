2bdf5777b566c4da8292b27d612ef107105ee748
Jongbin Jung <jungjongbin86@gmail.com>
2018-03-01 13:46:47 -0800

    Fix broken unit test for undi()

--- tests/testthat/test-run.R
+++ tests/testthat/test-run.R
@@ -1,22 +1,21 @@
 context("run")
 
-library(tidyverse)
-
 
 # Generate top-level test data --------------------------------------------
 N <- 2000
 set.seed(1)
 data <- data.frame(id = rep(1:N)) %>%
-  dplyr::mutate(m = rnorm(N, 0, 1),
-                x = factor(rep(c("red", "blue"), N/2)),
-                x = fct_relevel(x, "red"),
-                z = rnorm(N, m, 2),
-                c = rnorm(N, m, 2),
-                e1 = rnorm(N, 0, 0.05),
-                e2 = rnorm(N, 0, 0.05),
-                risk = 1 + 0.05*(x == "blue") + 0.25*z + c + e1,
-                a = inv_logit(risk + e2) > .5,
-                y = inv_logit(risk) > .5)
+  dplyr::mutate(
+    m = rnorm(N, 0, 1),
+    x = factor(rep(c("red", "blue"), N / 2)),
+    z = rnorm(N, m, 2),
+    c = rnorm(N, m, 2),
+    e1 = rnorm(N, 0, 0.05),
+    e2 = rnorm(N, 0, 0.05),
+    risk = 1 + 0.05 * (x == "blue") + 0.25 * z + c + e1,
+    a = inv_logit(risk + e2) > .5,
+    y = inv_logit(risk) > .5
+  )
 
 # Test basic constructor functionality ------------------------------------
 test_that("undi can generate a trivial object with custom values", {
