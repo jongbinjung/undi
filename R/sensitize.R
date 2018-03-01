0c6cbdb55c734aa94f41e6468480affbf94f9e2e
Jongbin Jung <jungjongbin86@gmail.com>
2018-03-01 14:10:04 -0800

    Implement selective comparison for sensitivity

--- R/sensitize.R
+++ R/sensitize.R
@@ -7,14 +7,19 @@
 #'   Details)
 #' @param d1 change in log-odds of response = 1 of treat = 1 and u = 1 (see
 #'   Details)
+#' @param compare (Optional) character vector of groups to compare; the data
+#'   will be filtered such that only specified groups are compared, and the
+#'   \code{grouping} variable will be refactored such that the levels preserve
+#'   the specified order, e.g., \code{compare = c("white", "black")} will make
+#'   \code{"white"} the base group
 #' @param ptreat (Optional) default value for probability of treatment; if
 #'   provided, it will override fitted values in \code{u$data}
 #' @param resp_ctl (Optional)
 #' @param resp_trt (Optional) default value for probability of response = 1
 #'   given each treatment regime (\code{ctl}, \code{trt}); useful for cases
 #'   where outcome under certain treatment regimes is deterministic (e.g.,
-#'   probability of finding illegal weapon if NOT frisked is 0); if provided,
-#'   it will override fitted values in \code{u$data}
+#'   probability of finding illegal weapon if NOT frisked is 0); if provided, it
+#'   will override fitted values in \code{u$data}
 #'
 #' @details All sensitivity parameters (\code{q, dp, d0, d1}) can be provided in
 #'   one of three formats, determined by the \code{length} of each argument:
@@ -22,7 +27,8 @@
 #'   observations (rows)} \item{if \code{length(arg) = }number of levels in
 #'   grouping variable}{each parameter setting applied to corresponding level in
 #'   group} \item{if \code{length(arg) = nrow(u$data)}}{each parameter applied
-#'   to corresponding rows}}
+#'   to corresponding rows}} Note that if \code{compare} is specified, the
+#'   number of grouping levels is effectively the length of \code{compare}
 #'
 #' @return \code{tidy} dataframe of second-stage model coefficients after
 #'   applying sensitivity parameters, with a nested column of sensitivity params
@@ -34,6 +40,7 @@ undisens <-
            dp,
            d0,
            d1,
+           compare = NULL,
            ptreat = NULL,
            resp_ctl = NULL,
            resp_trt = NULL) {
@@ -45,6 +52,32 @@ undisens <-
   wfit2 <- function(f, d, ...) u$fit2(f, d, w = weights)
 
   d <- u$data
+
+  # Filter and refactor data by grouping variable, as necessary
+  if (!is.null(compare)) {
+    # Check that compare is well defined
+    check_levels <- sapply(compare, function(x) x %in% levels(d[[u$grouping]]))
+    if (!all(check_levels)) {
+      stop("Groups specified in compare do not exist:\n\t",
+           compare[!check_levels],
+           "\nAvailable groups are:\n\t",
+           paste0(levels(d[[u$grouping]]), collapse = ", "))
+    }
+
+    target_group_ind <- d[[u$grouping]] %in% compare
+
+    d <- d[target_group_ind, ]
+    d[[u$grouping]] <- forcats::fct_drop(d[[u$grouping]])
+    d[[u$grouping]] <- forcats::fct_relevel(d[[u$grouping]], compare)
+  }
+
+  if (is.factor(d[[u$grouping]])) {
+    message(sprintf("Comparing %s=%s against %s={%s}",
+                    u$grouping,
+                    levels(d[[u$grouping]])[1],
+                    u$grouping,
+                    paste0(levels(d[[u$grouping]])[-1], collapse = ", ")))
+  }
 
   # Initialize columns required for sensitize()
   d$treat <- d[[u$treatment]]
