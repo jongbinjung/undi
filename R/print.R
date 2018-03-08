a5b477048d6d9d168832b9ae9621243c7998cdb1
Jongbin Jung <jungjongbin86@gmail.com>
2018-03-08 12:14:26 -0800

    Stop printing model type

--- R/print.R
+++ R/print.R
@@ -10,29 +10,26 @@ print.policy <- function(x, ...) {
   d <- x$data
 
   train_ind <- d$fold__ == "train"
-  ctl_train <- train_ind & d[[x$treatment]] == 0
-  trt_train <- train_ind & d[[x$treatment]] == 1
+  ctl_train <- train_ind & (d[[x$treatment]] == 0)
+  trt_train <- train_ind & (d[[x$treatment]] == 1)
 
-  ctl_test <- !train_ind & d[[x$treatment]] == 0
-  trt_test <- !train_ind & d[[x$treatment]] == 1
+  ctl_test <- (!train_ind) & (d[[x$treatment]] == 0)
+  trt_test <- (!train_ind) & (d[[x$treatment]] == 1)
 
   s <- paste("Models have train/test AUC",
-             "\tptreat(%s): %.2f / %.2f",
-             "\tresp_ctl(%s): %.2f / %.2f",
-             "\tresp_trt(%s): %.2f / %.2f",
+             "\tptreat  : %.2f / %.2f",
+             "\tresp_ctl: %.2f / %.2f",
+             "\tresp_trt: %.2f / %.2f",
              "",
              sep = "\n")
 
   cat(
     sprintf(
       s,
-      class(x$m_ptrt),
       .compute_auc(d[train_ind, "ptrt__"], d[train_ind, x$treatment]) * 100,
       .compute_auc(d[!train_ind, "ptrt__"], d[!train_ind, x$treatment]) * 100,
-      class(x$m1_ctl),
       .compute_auc(d[ctl_train, "resp_ctl__"], d[ctl_train, x$outcome]) * 100,
       .compute_auc(d[ctl_test, "resp_ctl__"], d[ctl_test, x$outcome]) * 100,
-      class(x$m1_trt),
       .compute_auc(d[trt_train, "resp_trt__"], d[trt_train, x$outcome]) * 100,
       .compute_auc(d[trt_test, "resp_trt__"], d[trt_test, x$outcome]) * 100
     )
