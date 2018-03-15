60760956a55a6a246978d1ab1ddbda97e5aed6be
Jongbin Jung <jungjongbin86@gmail.com>
2018-03-15 10:33:53 -0700

    Return data frame from summary.optimsens

--- R/summary.R
+++ R/summary.R
@@ -1,12 +1,15 @@
 #' Summary of optimsens object
 #'
-#' @param x optimsens object
+#' @param object optimsens object
 #' @param ... other stuff
 #'
-summary.optimsens <- function(x, ...) {
-  format(x$results %>%
+#' @return summary data frame with min/max values of each group term
+#'
+#' @export
+summary.optimsens <- function(object, ...) {
+  object$results %>%
     tidyr::separate(tag, into = c("group", "bound"), sep = "_") %>%
     dplyr::select(term, estimate, bound) %>%
     tidyr::spread(bound, estimate) %>%
-    dplyr::select(term, min, max))
+    dplyr::select(term, min, max)
 }
