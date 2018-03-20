a0a3eca6e0ad10724d41787479fc53edea24ddbd
Jongbin Jung <jungjongbin86@gmail.com>
2018-03-20 15:12:15 -0700

    Add glmnet models

--- R/models.R
+++ R/models.R
@@ -18,11 +18,23 @@ models <- function() {
       pred = function(m, d, f)
         stats::predict.glm(m, newdata = d, type = "response")
     ),
+    lasso = list(
+      fit = function(f, d, ...)
+        fit_glmnet(f, d, family = "binomial", alpha = 1, ...),
+      pred = function(m, d, f)
+        predict4mm(m, d, f, type = "response", s = "lambda.min")
+    ),
+    ridge = list(
+      fit = function(f, d, ...)
+        fit_glmnet(f, d, family = "binomial", alpha = 0, ...),
+      pred = function(m, d, f)
+        predict4mm(m, d, f, type = "response", s = "lambda.min")
+    ),
     sgd = list(
       fit = function(f, d, ...)
         fit_sgd(f, d, model = "glm",
                 model.control = list(family = "binomial"), ...),
-      pred = function(m, d, f) predict_sgd(m, d, f, type = "response")
+      pred = function(m, d, f) predict4mm(m, d, f, type = "response")
     )
   )
 }
@@ -37,6 +49,65 @@ models <- function() {
 #' @return an \code{sgd} object
 #' @export
 fit_sgd <- function(f, d, ...) {
+  dm <- .get_mm(f, d)
+
+  Y <- dm$Y
+  x <- dm$x
+
+  scaled <- .safe_scale(x)
+  x[, scaled$ind] <- scaled$scaled
+  m <- sgd::sgd(x, Y, ...)
+  m$coef[scaled$ind] <-
+    (m$coef[scaled$ind] + attr(scaled$scaled, "scaled:center")) *
+    attr(scaled$scaled, "scaled:scale")
+
+  return(m)
+}
+
+
+#' Wrapper for fitting \code{\link[glmnet]{glmnet}} models from a formula
+#'
+#' @param f formula
+#' @param d data object
+#' @param ... additional arguments for \code{\link[glmnet]{cv.glmnet}}
+#'
+#' @return an \code{\link[glmnet]{cv.glmnet}} object
+#' @export
+fit_glmnet <- function(f, d, ...) {
+  dm <- .get_mm(f, d)
+
+  Y <- dm$Y
+  x <- dm$x
+
+  m <- glmnet::cv.glmnet(x, Y, ...)
+
+  return(m)
+}
+
+#' Wrapper to compute predictions from a model matrix
+#'
+#' @param m model object
+#' @param d data frame to generate predictions on
+#' @param f model formula; used to transform \code{d} to a \code{design matrix}
+#' @param ... other arguments to pass to the predict function
+#'
+#' @return generated predictions
+#' @export
+predict4mm <- function(m, d, f, ...) {
+  mm <- stats::model.matrix(f, d)
+
+  stats::predict(m, mm, ...)
+}
+
+
+#' Get model matrix and response given a formula and data frame
+#'
+#' @param f model formula
+#' @param d data frame
+#'
+#' @return a list with \code{Y} and \code{x}, the response and design matrix,
+#'   respectively
+.get_mm <- function(f, d) {
   mf <- stats::model.frame(f, d)
 
   # Extract Y values
@@ -57,29 +128,5 @@ fit_sgd <- function(f, d, ...) {
     stop("No valid terms found in data for specified formula: ", format(f))
   }
 
-  scaled <- .safe_scale(x)
-  x[, scaled$ind] <- scaled$scaled
-  m <- sgd::sgd(x, Y, ...)
-  m$coef[scaled$ind] <-
-    (m$coef[scaled$ind] + attr(scaled$scaled, "scaled:center")) *
-    attr(scaled$scaled, "scaled:scale")
-
-  return(m)
-}
-
-#' Wrapper to compute predictions for sgd model
-#'
-#' @param m sgd object
-#' @param d data frame to generate predictions on
-#' @param f formula used in sgd object; used to transform \code{d} to a
-#'   \code{design matrix}
-#' @param type the type of prediction to return (default: response)
-#' @param ... other arguments to pass to predict.sgd
-#'
-#' @return prediction for sgd model
-#' @export
-predict_sgd <- function(m, d, f, type = "response", ...) {
-  mm <- stats::model.matrix(f, d)
-
-  stats::predict(m, mm, type, ...)
+  list(Y = Y, x = x)
 }
